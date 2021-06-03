c43ac9d69eacb721400e0d8bc744a7519c9ffd47
Lilas-w <wangziweipku@pku.edu.cn>
2021-06-03 20:13:44 +0800

    birthday reminder

---
+++ 01-birthday-reminder/setup/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'xiaoxue',
+    age: 24,
+    image:
+      'https://tse3-mm.cn.bing.net/th/id/OIP.4hA2aZi4WEllhHfz7icVwQAAAA?pid=ImgDet&rs=1',
+  },
+  {
+    id: 2,
+    name: 'tongtong',
+    age: 23,
+    image:
+      'https://up.enterdesk.com/edpic/3a/0c/55/3a0c55b3113f38ff63d9919603e635ac.png',
+  },
+  {
+    id: 3,
+    name: 'Larry Little',
+    age: 36,
+    image:
+      'https://tse2-mm.cn.bing.net/th/id/OIP.ZFsOxD1JS1kBWuvTENvNZgHaHa?pid=ImgDet&rs=1',
+  },
+  {
+    id: 4,
+    name: 'Sean Walsh',
+    age: 34,
+    image:
+      'https://tse4-mm.cn.bing.net/th/id/OIP.5m7Zbhnwrj7423fwd8is9ADlEQ?w=155&h=188&c=7&o=5&dpr=1.5&pid=1.7',
+  },
+  {
+    id: 5,
+    name: 'Lola Gardner',
+    age: 29,
+    image:
+      'https://n.sinaimg.cn/news/crawl/76/w550h326/20190615/09c2-hymscpq5000443.jpg',
+  },
+];
