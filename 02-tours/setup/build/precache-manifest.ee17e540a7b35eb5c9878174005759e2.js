5ab693e95582b37a45e829b5b66505322f0d355a
Lilas-w <wangziweipku@pku.edu.cn>
2021-06-09 10:30:01 +0800

    update 02

---
+++ 02-tours/setup/build/precache-manifest.ee17e540a7b35eb5c9878174005759e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d9bea689d6bd0202f892721e0028b19",
+    "url": "./index.html"
+  },
+  {
+    "revision": "df56f2fb0c1753176934",
+    "url": "./static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "./static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df56f2fb0c1753176934",
+    "url": "./static/js/main.54187924.chunk.js"
+  },
+  {
+    "revision": "634688380851e0148535",
+    "url": "./static/js/runtime-main.9a51be4d.js"
+  }
+]);
