3bc66296f4d36d2df9f55eafbf81f536545f2a9c
Lilas-w <wangziweipku@pku.edu.cn>
2021-08-29 16:32:29 +0800

    build 10&11

---
+++ 10-grocery-bud/setup/build/precache-manifest.6d5e86c90819416709237fc252738501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95461bb00203ee2842f88db0fd5fb953",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea587b92d9fc7f979354",
+    "url": "/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea587b92d9fc7f979354",
+    "url": "/static/js/main.0adcdae8.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
