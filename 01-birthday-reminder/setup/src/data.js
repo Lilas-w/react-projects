04fe90e3cf92feda0c293d7177d56a021f4abe4a
Lilas-w <wangziweipku@pku.edu.cn>
2022-04-19 22:50:08 +0800

    update 01

--- 01-birthday-reminder/setup/src/data.js
+++ 01-birthday-reminder/setup/src/data.js
@@ -8,27 +8,20 @@ export default [
   },
   {
     id: 2,
-    name: 'tongtong',
-    age: 23,
-    image:
-      'https://cn.bing.com/images/search?view=detailV2&ccid=rqRQuN48&id=8AEA69332AA1DC6B2DB6D139F44D3AC938634AA9&thid=OIP.rqRQuN48MORSPUrsUYCG_AAAAA&mediaurl=https%3a%2f%2fpic2.ntimg.cn%2ffile%2f20210121%2f32667518_193757538387_1.jpg&exph=150&expw=143&q=%e6%98%9f%e9%bb%9b%e9%9c%b2&simid=608031154531812251&FORM=IRPRST&ck=D591FA64CEB0B610334064E4FBEC6380&selectedIndex=0&qft=+filterui%3aimagesize-small&ajaxhist=0&ajaxserp=0',
-  },
-  {
-    id: 3,
     name: 'Larry Little',
     age: 36,
     image:
       'https://tse2-mm.cn.bing.net/th/id/OIP.ZFsOxD1JS1kBWuvTENvNZgHaHa?pid=ImgDet&rs=1',
   },
   {
-    id: 4,
+    id: 3,
     name: 'Sean Walsh',
     age: 34,
     image:
       'https://tse4-mm.cn.bing.net/th/id/OIP.5m7Zbhnwrj7423fwd8is9ADlEQ?w=155&h=188&c=7&o=5&dpr=1.5&pid=1.7',
   },
   {
-    id: 5,
+    id: 4,
     name: 'Lola Gardner',
     age: 29,
     image:
